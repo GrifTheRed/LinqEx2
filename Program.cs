7f19c523a9497fc7079f9943fe93c03a47a3c63a
Derek Griffin <griffinderek6@gmail.com>
2022-08-29 08:25:44 -0600

    finished

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGameNames = new List<string>
+            {"Call Of Duty", "Madden", "Skyrim", "The Show", "Forza Horizions" };
+
+            //order by name length
+
+            videoGameNames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+
+            
+        }
+    }
+}
+
